f8af92fb6a8e1204200828c5fc12cb4484c65d6a
roman <smtn.roman@gmail.com>
2022-09-18 17:34:53 +0200

    ganerated Project table/model

---
+++ db/migrate/20220918153357_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false, default: ''
+      t.integer :position, null: false
+
+      t.timestamps
+    end
+  end
+end
