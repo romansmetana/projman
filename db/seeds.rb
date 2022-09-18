10957ccab8eadb415cfa74e2385311d62ec4a9e4
roman <smtn.roman@gmail.com>
2022-09-18 18:21:36 +0200

    added seeds for all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(
+    name: 'Roman',
+    surname: 'Smetana',
+    email: 'roman@gmail.com',
+    password: 'password'
+)
+
+project = Project.create(
+    title: 'To-do list',
+    position: 1,
+    user_id: user.id
+)
+
+tag = Tag.create(
+    title: 'test',
+    user_id: user.id
+)
+
+task = Task.create(
+    title: 'Make to-do list',
+    description: 'You need to created todo model',
+    is_done: false,
+    user_id: user.id,
+    project_id: project.id
+
+)
+
+TaskTag.create(
+    task_id: task.id,
+    tag_id: tag.id
+)
