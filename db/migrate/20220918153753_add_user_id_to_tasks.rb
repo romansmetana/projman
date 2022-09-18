acb16b1589d8b499eb51966bd07964a1104d3f50
roman <smtn.roman@gmail.com>
2022-09-18 17:40:30 +0200

    added user_id to tasks

---
+++ db/migrate/20220918153753_add_user_id_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :tasks, :user, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_18_152824) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_153753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.integer "position", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", default: "", null: false
@@ -21,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_18_152824) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
