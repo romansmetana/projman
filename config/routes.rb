732063766978c8e2e18728992edcd385da31c9f9
roman <smtn.roman@gmail.com>
2022-09-19 12:16:09 +0200

    ganerated Projects controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :projects
+  
   # Defines the root path route ("/")
   # root "home#index"
 end
