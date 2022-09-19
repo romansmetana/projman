58c5ad3ed7802901fbd50ae6e26b5867b782a999
roman <smtn.roman@gmail.com>
2022-09-19 15:55:41 +0200

    Project edit using turbo_stream

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show edit update destroy]
   def index
     @projects = current_user.projects.all
   end
@@ -17,18 +18,36 @@ class ProjectsController < ApplicationController
       if @project.save
         format.turbo_stream
       else
-
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@project)}_form", partial: 'form',
+                                                                                        locals: { project: @project })
+        end
       end
     end
   end
 
   def edit; end
 
-  def update; end
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to root_path }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@project)}_form", partial: 'form',
+                                                                                        locals: { project: @project })
+        end
+      end
+    end
+  end
 
   def destroy; end
 
   private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit(:title, :position)
