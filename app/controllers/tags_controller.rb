e46991404dc3f7260cf5228866aab7ad2578e424
roman <smtn.roman@gmail.com>
2022-09-20 13:38:35 +0200

    i18n for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,24 @@
 class TagsController < ApplicationController
   def index
+    @tags = current_user.tags.all
   end
 
   def new
   end
 
   def create
+    @tag = current_user.tags.build(tag_params)
+    respond_to do |format|
+      if @tag.save
+        flash[:success] = t('controllers.tag.success.create')
+        format.turbo_stream
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@tag)}_form", partial: 'form',
+                                                                                     locals: { tag: @tag })
+        end
+      end
+    end
   end
 
   def edit
@@ -15,5 +28,11 @@ class TagsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:title, :taks_id)
   end
 end
